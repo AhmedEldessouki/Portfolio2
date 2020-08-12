958bc45f92ba1cd6318d0b24733f3a3e5c8bece4
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2020-08-12 18:31:19 +0300

    adding MDX

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,8 @@ module.exports = {
       options: {
         preset: "@theme-ui/preset-funk",
       },
-    }
+    },
+    `gatsby-plugin-mdx`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,7 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/about/">ABOUT</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
