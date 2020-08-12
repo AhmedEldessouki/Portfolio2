79a37e100b1285a60ef4ef7b988d79d7604fee0d
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2020-08-12 19:05:03 +0300

    adding and setting up Husky, Eslint, and Prettier .. also added and configured  MDX

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,9 +48,9 @@ module.exports = {
         preset: "@theme-ui/preset-funk",
       },
     },
-    `gatsby-plugin-mdx`
+    `gatsby-plugin-mdx`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
